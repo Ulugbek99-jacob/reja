bd790ab7ca4dd73dc87fb7e82b9b3f7386d59380
Ulugbek99-jacob <eshboltayevulughbek@gmail.com>
2025-02-21 15:54:36 +0900

    feat: develop author router ejs  page

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 // 1: Kirish code 
 app.use(express.static("public"));
@@ -17,6 +27,10 @@ app.set("view engine", "ejs");
 // app.get("/", function(req, res) {
 //  res.end(`<h1 style="background : red">HELLO WORLD by Jacob</h1>`);
 // });
+
+app.get("/author", (req, res) => {
+    res.render("author", { user: user });
+});
 
 app.post("/create-item", (req, res) => {
     console.log(req);
