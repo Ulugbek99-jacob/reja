0be79c9e20a45821131b6c4a527b0e82fe6646fc
Ulugbek99-jacob <eshboltayevulughbek@gmail.com>
2025-02-26 16:23:24 +0900

    B task and fix: refactoring

--- train.js
+++ train.js
@@ -16,37 +16,37 @@
 
 
 // CALLBACK function
-console.log("Jack Ma maslahatlari");
-const list = [
+// console.log("Jack Ma maslahatlari");
+// const list = [
     // "yaxshi talaba boling", // 0-20
     // "togri boshliq tanlang va koproq hato qiling", // 20-30
     // "uzingizga ishlashingizni boshlang", // 30-40
     // "siz kuchli bolgan narsalarni qiling", // 40-50
     // "yoshlarga investitsiya qiling", // 50-60
     // "endi dam oling, foydasi yoq endi", // 60
-];
-function maslahatBering(a, callback) {
-    if(typeof a !=="number") callback("insert a number",null);
-    else if(a <= 20) callback(null,list[0]);
-    else if(a > 20 && a <= 30) callback(null, list[1]);
-    else if(a > 30 && a <= 30) callback(null, list[2]);
-    else if(a > 40 && a <= 50) callback(null, list[3]);
-    else if(a > 50 && a <= 60) callback(null, list[4]);
-    else {
-        setTimeout(function(){
-            callback(null, list[5]);    
-        }, 5000);
-    }
-}
+// ];
+// function maslahatBering(a, callback) {
+//     if(typeof a !=="number") callback("insert a number",null);
+//     else if(a <= 20) callback(null,list[0]);
+//     else if(a > 20 && a <= 30) callback(null, list[1]);
+//     else if(a > 30 && a <= 30) callback(null, list[2]);
+//     else if(a > 40 && a <= 50) callback(null, list[3]);
+//     else if(a > 50 && a <= 60) callback(null, list[4]);
+//     else {
+//         setTimeout(function(){
+//             callback(null, list[5]);    
+//         }, 5000);
+//     }
+// }
 
-console.log('passed here 0');
-maslahatBering(65, (err, data) => {
-    if(err) console.log('ERROR:', err);
-    else {
-        console.log('javob:', data);
-    }
-})
-console.log('passed here 1');
+// console.log('passed here 0');
+// maslahatBering(65, (err, data) => {
+//     if(err) console.log('ERROR:', err);
+//     else {
+//         console.log('javob:', data);
+//     }
+// })
+// console.log('passed here 1');
 
 
 // ASYNC function
@@ -113,3 +113,29 @@ console.log('passed here 1');
 //   console.log(javob);
 // }
 // run();
+
+// B-TASK
+function countDigits(word) {
+    let count = 0;
+   for (let i=0; i<word.length; i++){
+     if (word[i] >= "0" && word[i] <= "9"){  
+     // console.log(word[i])
+     count=count+1;
+     }
+   }
+  return count;   
+  }
+  
+  
+  
+  console.log(countDigits("ad2a54y79wet0sfgb9"));
+  
+
+
+
+
+
+
+
+
+    
